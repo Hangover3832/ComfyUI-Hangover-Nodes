fe714ac9fe0d674ef430eb9903376fe594fb9a24
AlexL <github@leual.ch>
2024-01-07 22:09:10 +0100

    update

--- image_scale_bounding_box.py
+++ image_scale_bounding_box.py
@@ -17,11 +17,11 @@ class ImageScaleBoundingBox:
                     "box_width": ("INT", {"default": 512, "min": 1, "max": MAX_RESOLUTION, "step": 1}),
                     "box_height": ("INT", {"default": 512, "min": 1, "max": MAX_RESOLUTION, "step": 1})}}
 
-    def upscale(self, image, upscale_method, width, height):
+    def upscale(self, image, upscale_method, box_width, box_height):
         samples = image.movedim(-1,1)
         w = round(samples.shape[3])
         h = round(samples.shape[2])
-        scale_by = min(width/w, height/h)
+        scale_by = min(box_width/w, box_height/h)
         new_width = round(w * scale_by)
         new_height = round(h * scale_by)
         s = comfy.utils.common_upscale(samples, new_width, new_height, upscale_method, "disabled")
